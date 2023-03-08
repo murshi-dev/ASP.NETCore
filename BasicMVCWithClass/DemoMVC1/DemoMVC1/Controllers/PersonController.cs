1c19bef3303bd1b00afb70b9117315aa95839269
murshi <90964789+murshi-dev@users.noreply.github.com>
2023-03-08 14:15:44 +0800

    c
    c

---
+++ BasicMVCWithClass/DemoMVC1/DemoMVC1/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using DemoMVC1.Models;
+namespace DemoMVC1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PersonInfo()
+        {
+            Person person = new Person();
+            person.PersonName = "Jane";
+            person.PersonAge= 30;
+            person.PersonLocation = "Penang";
+
+            return View(person);
+        }
+    }
+}
---
+++ BasicMVCWithClass/DemoMVC1/DemoMVC1/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoMVC1.Models
+{
+	public class Person
+	{
+		public string PersonName { get; set; }
+		public int PersonAge { get; set; }
+		public string PersonLocation { get; set; }
+
+
+
+	}
+}

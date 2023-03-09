99cf3c95f70006778e183a7ded992a03d532858a
murshi <90964789+murshi-dev@users.noreply.github.com>
2023-03-09 14:53:27 +0800

    c
    c

---
+++ BasicMVCWithClass/DemoMVC3/DemoMVC3/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using DemoMVC3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC3.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PersonInfo()
+        {
+
+            ViewPerson myperson = new ViewPerson();
+            myperson.Persons = GetPersons();
+            return View(myperson);
+        }
+        public List<Person> GetPersons()//create objects and group them in a List 
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person { PersonName = "Amy", PersonAge = 30, PersonLocation = "GeorgeTown" });
+            persons.Add(new Person { PersonName = "Jane", PersonAge = 40, PersonLocation = "Batu Kawan" });
+            persons.Add(new Person { PersonName = "John", PersonAge = 50, PersonLocation = "bayan Lepas" });
+            return persons;
+        }
+
+
+    }
+}
---
+++ BasicMVCWithClass/DemoMVC3/DemoMVC3/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoMVC3.Models
+{
+	public class Person
+	{
+		public string PersonName { get; set; }
+		public int PersonAge { get; set; }
+		public string PersonLocation { get; set; }
+	}
+	public class ViewPerson
+	{
+		//use IEnumerable to loop over the person list using for each
+		//can loop over an array or a list or any custom data types
+		public IEnumerable<Person> Persons { get; set; }
+
+	}
+
+
+}

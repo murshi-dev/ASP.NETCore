c2cb9c0380f4d705b92bd2d25b567376a011c3d8
murshi <90964789+murshi-dev@users.noreply.github.com>
2023-03-09 15:06:49 +0800

    c
    c

---
+++ ASPForms/Creating Forms Using Tag Helpers/FormsValidation/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormsValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormsValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserViewModel model)
+        {
+            //Traditional if..else
+            //if (string.IsNullOrEmpty(model.Name))
+            //{
+            //    ModelState.AddModelError("Name", "Please Enter Name");
+            //}
+            //if (string.IsNullOrEmpty(model.Username))
+            //{
+            //    ModelState.AddModelError("Username", "Please Enter Username");
+            //}
+
+            //
+            if (ModelState.IsValid)
+            {
+                //TO DO:
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPForms/FormsValidation-ServerSide/FormsValidation/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormsValidation.Models
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage ="Please Enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Confirm Password")]
+        [Compare("Password",ErrorMessage ="Confirm Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+        public string Address { get; set; }
+
+        [RegularExpression(@"^[789]\d{9}$",ErrorMessage = "Please Enter Correct Contact")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Please Accept Terms")]
+        public bool Terms { get; set; }
+    }
+}

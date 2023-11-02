051383096f7a4e227f633bf27dd35a49dbe91100
murshi <90964789+murshi-dev@users.noreply.github.com>
2023-11-02 15:48:04 +0800

    c
    c

---
+++ BasicMVCWithClass/DemoMVC4/WebApplication2/WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿//create object for the Person model class and 
+//provide value to the properties id name and location
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+	public class PersonController : Controller
+	{
+		public IActionResult Info()
+		{
+			//create object
+			Person person = new Person();
+			//using person object fill deatils of properties
+			person.Id = 1001;
+			person.Name = "Jane";
+			person.Location = "Penang";
+			//return this person object to view
+			return View(person);
+		}
+	}
+}

5358e996e8faefa6056dca0085e9b917cc408909
murshi <90964789+murshi-dev@users.noreply.github.com>
2024-06-28 10:37:13 +0800

    c
    c

---
+++ BasicMVCWithClass/May24/DemoMVC/DemoMVC/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    //PersonController inherits from Controller class
+    public class PersonController : Controller
+    {
+        //IActionResult - interface --here it rerepsents
+        //the result of an action method - Index()
+        public IActionResult Index()
+        {
+            //creating object for the Person class
+            Person person = new Person();
+            //assign values to the properties
+            person.Id = 1001;
+            person.Name = "Jane";
+            person.Location = "Penang";
+            person.Age= 30;
+            person.ContactNumber = "01234567";
+            //return the object person
+            return View(person);
+        }
+    }
+}
---
+++ BasicMVCWithClass/May24/DemoMVC/DemoMVC/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoMVC.Models
+{
+    public class Person
+    {
+        //list all the properties for the Person class
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Age { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
